5ddcd9ec829a5e29c202be4e103ec84a3f05d217
yakuhito <y@kuhi.to>
2024-03-02 17:35:44 +0100

    update .gitignore

--- commands/rekey.py
+++ commands/rekey.py
@@ -6,6 +6,8 @@ from drivers.multisig import *
 from drivers.portal import get_portal_rekey_delegated_puzzle
 from commands.multisig import get_cold_key_signature
 
+PORTAL_COIN_ID_SAVE_FILE = "last_spent_portla_coinid"
+
 @click.group()
 def rekey():
     pass
@@ -55,10 +57,41 @@ def sign_tx(
 
 
 @rekey.command()
-@click.option('--sigs', required=True, help='Signatures, separated by commas')
-@click.option('--new-keys', required=True, help='New set of cold keys, separated by commas')
-@click.option('--new-threshold', required=True, help='New threshold required for updates')
+@click.option('--new-message-keys', required=True, help='New set of hot keys, separated by commas')
+@click.option('--new-message-threshold', required=True, help='New threshold required for messages')
+@click.option('--new-update-keys', required=True, help='New set of cold keys, separated by commas')
+@click.option('--new-update-threshold', required=True, help='New threshold required for updates')
+@click.option('--sigs', required=True, help='Signature list - in the form: list of {validator_index-sig}, elements separated by comma (,)')
 @async_func
 @with_node
-async def broadcast_spend():
-    pass
+async def broadcast_spend(
+    new_message_keys: str,
+    new_message_threshold: int,
+    new_update_keys: str,
+    new_update_threshold: int,
+    sigs: str
+):
+    new_message_keys: List[G1Element] = [G1Element.from_bytes(bytes.fromhex(key)) for key in new_message_keys.split(',')]
+    new_update_keys: List[G1Element] = [G1Element.from_bytes(bytes.fromhex(key)) for key in new_update_keys.split(',')]
+    new_message_threshold = int(new_message_threshold)
+    new_update_threshold = int(new_update_threshold)
+
+    portal_launcher_id = bytes.fromhex(get_config_item(["xch", "portal_launcher_id"]))
+
+    current_message_threshold = int(get_config_item(["xch", "portal_threshold"]))
+    current_message_keys = [G1Element.from_bytes(bytes.fromhex(pk)) for pk in get_config_item(["xch", "portal_keys"]).split(',')]
+    current_update_threshold = int(get_config_item(["xch", "multisig_threshold"]))
+    current_update_keys = [G1Element.from_bytes(bytes.fromhex(pk)) for pk in get_config_item(["xch", "multisig_keys"]).split(',')]
+
+    updater_delegated_puzzle = get_portal_rekey_delegated_puzzle(
+        portal_launcher_id,
+        current_message_threshold,
+        current_message_keys,
+        new_message_threshold,
+        new_message_keys,
+        current_update_threshold,
+        current_update_keys,
+        new_update_threshold,
+        new_update_keys
+    )
+    updater_delegated_puzzle_hash = updater_delegated_puzzle.get_tree_hash()
