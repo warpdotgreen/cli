466be0d06a3e92acb87c5e195b9180673966a0f4
yakuhito <y@kuhi.to>
2024-03-29 20:29:45 +0200

    we're making this immutable boisss

--- contracts/WrappedCAT.sol
+++ contracts/WrappedCAT.sol
@@ -5,19 +5,47 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WrappedCAT is Ownable, ERC20, ERC20Permit {
+contract WrappedCAT is ERC20, ERC20Permit, IPortalMessageReceiver {
+    address public immutable portal;
+    uint64 public immutable fee; // fee / 10000 %
+    uint64 public immutable mojoToTokenRatio; // token amount on eth = mojos on Chia * mojoToTokenRatio
+    bytes32 public immutable lockerPuzzleHash;
+    bytes32 public immutable unlockerPuzzleHash;
+
     constructor(
         string memory _name,
-        string memory _symbol
-    ) Ownable(msg.sender) ERC20(_name, _symbol) ERC20Permit(_name) {}
-
-    function mint(address _to, uint256 _amount) public onlyOwner {
-        _mint(_to, _amount);
+        string memory _symbol,
+        address _portal,
+        uint64 _fee,
+        uint64 _mojoToTokenRatio,
+        bytes32 _lockerPuzzleHash,
+        bytes32 _unlockerPuzzleHash
+    ) Ownable(msg.sender) ERC20(_name, _symbol) ERC20Permit(_name) {
+        portal = _portal;
+        fee = _fee;
+        mojoToTokenRatio = _mojoToTokenRatio;
+        lockerPuzzleHash = _lockerPuzzleHash;
+        unlockerPuzzleHash = _unlockerPuzzleHash;
     }
 
-    function burn(address _from, uint256 _amount) public onlyOwner {
-        _burn(_from, _amount);
+    function receiveMessage(
+        bytes32 /* _nonce */,
+        bytes3 _source_chain,
+        bytes32 _source,
+        bytes32[] memory _contents
+    ) public {
+        require(
+            msg.sender == portal &&
+                _source == lockerPuzzleHash &&
+                _source_chain == bytes3("xch"),
+            "!msg"
+        );
+
+        uint256 amount = uint256(_contents[1]) * mojoToTokenRatio;
+        uint256 transferFee = (amount * fee) / 10000;
+
+        _mint(address(uint160(uint256(_contents[0]))), amount - transferFee);
+        _mint(portal, transferFee);
     }
 }
