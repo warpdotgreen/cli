5c0ffebba383950825b27a4f2af961751bf81f73
yakuhito <y@kuhi.to>
2024-05-21 10:46:52 +0300

    update hardhat version in example

--- hardhat.config.example.ts
+++ hardhat.config.example.ts
@@ -4,7 +4,7 @@ import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.23",
     settings: {
       optimizer: {
         enabled: true,
